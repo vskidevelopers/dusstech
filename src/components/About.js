99c80e1bbb2806baee4c3efa4dba2be871f4d21f
Ben <71015535+vskidevelopers@users.noreply.github.com>
2022-10-03 06:02:27 +0300

    Updated About Us section

--- src/components/About.js
+++ src/components/About.js
@@ -41,8 +41,9 @@ function About() {
                     <div className="h-100">
                         <h6 className="section-title bg-white text-start text-primary px-3">About Us</h6>
                         <h1 className="display-6 mb-4">#1 Branding Solution With <span className="text-primary">10 Years</span> Of Experience</h1>
-                        <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                        <p className="mb-4">Stet no et lorem dolor et diam, amet duo ut dolore vero eos. No stet est diam rebum amet diam ipsum. Clita clita labore, dolor duo nonumy clita sit at, sed sit sanctus dolor eos.</p>
+                        <p>Dusstech Brands is a Kenyan registered creative agency with a diverse portfolio of clients in search for top notch printing, branding, and marketing services locally and across the border. </p>
+                        <p className="mb-4">Equipped with a team of graphic designers, content creators, web developers and copywriters, we have established Trust, Passion, and Leadership with all our clients in the quest to
+                            meet their specific organizational goals.</p>
                         <div className="d-flex align-items-center mb-4 pb-2">
                             <img className="flex-shrink-0 rounded-circle" src={placeholder}alt="" style={{width: '50px', height: '50px'}}/>
                             <div className="ps-4">
